cb1f02f1175d21810dd47fe906e7865d5e891e5e
jungyoonn <sophia7625@naver.com>
2025-04-04 19:25:43 +0900

    백엔드 첫 커밋

---
+++ src/main/java/com/eeerrorcode/lottomate/LottomateApplication.java
@@ -0,0 +1,13 @@
+package com.eeerrorcode.lottomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottomateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeerrorcode/lottomate/LottomateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeerrorcode.lottomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
