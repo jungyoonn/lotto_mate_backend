c2fee4ef6c27c94f19d539e73a7eb14c17e6cb23
DaHnDell <khanman1208@gmail.com>
2025-04-16 18:13:30 +0900

    서비스로직(전체조회, 마지막 회차 조회) 추가

---
+++ src/main/java/com/eeerrorcode/lottomate/service/LottoCrawlerService.java
@@ -0,0 +1,17 @@
+package com.eeerrorcode.lottomate.service;
+
+import org.openqa.selenium.WebDriver;
+
+public interface LottoCrawlerService {
+
+    // 크론 활용한 최신 추첨 번호 가져오기(스케줄러)
+    public void crawlLatest();
+
+    // 최신 추첨 번호 가져오기 위한 최신 회차 정보 가져오기(동행복권 크롤링)
+    public Integer getLatestRound();
+
+    // 크롤링한 정보 기반 저장 및 쿼리 업데이트(동행복권 크롤링)
+    public void crawlAndSave(WebDriver driver, int Round);
+
+    public void crawlAll();
+} 
