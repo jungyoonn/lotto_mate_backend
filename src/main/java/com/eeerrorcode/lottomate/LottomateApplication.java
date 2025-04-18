89df4706f5cda54496aa02ea2a83ccc5225291b3
jungyoonn <sophia7625@naver.com>
2025-04-18 15:22:29 +0900

    JPA 어노테이션 추가

--- src/main/java/com/eeerrorcode/lottomate/LottomateApplication.java
+++ src/main/java/com/eeerrorcode/lottomate/LottomateApplication.java
@@ -2,8 +2,10 @@ package com.eeerrorcode.lottomate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LottomateApplication {
 
 	public static void main(String[] args) {
