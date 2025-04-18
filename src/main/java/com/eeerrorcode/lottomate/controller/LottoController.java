9770c287f3c8bf94c014fbdaaed495b7969a840e
DaHnDell <khanman1208@gmail.com>
2025-04-18 14:57:06 +0900

    응답 DTO 생성

--- src/main/java/com/eeerrorcode/lottomate/controller/LottoController.java
+++ src/main/java/com/eeerrorcode/lottomate/controller/LottoController.java
@@ -3,12 +3,15 @@ package com.eeerrorcode.lottomate.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eeerrorcode.lottomate.service.LottoCrawlerService;
-
+import com.eeerrorcode.lottomate.domain.dto.ApiResponse;
+import com.eeerrorcode.lottomate.domain.dto.lotto.LottoResultResponse;
+import com.eeerrorcode.lottomate.service.lotto.*;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
@@ -17,11 +20,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LottoController {
 
     private final LottoCrawlerService crawlerService;
+    private final LottoResultService resultService;
 
+
+    // 전체 크롤링 메서드(DB 업데이트용)
     @PostMapping("/crawlall")
     public ResponseEntity<?> crawlAll() {
         crawlerService.crawlAll();
         return ResponseEntity.ok("전체 회차 크롤링을 성공적으로 완료했습니다.");
+    }
+    
+    // DB 상태 확인 메서드
+    @GetMapping("/verify")
+    public ResponseEntity<ApiResponse<LottoResultResponse>> verifyData() {
+        LottoResultResponse verifiedResponse = resultService.getLottoStatus();
+        return ResponseEntity.ok(new ApiResponse<>("데이터 검증 완료", verifiedResponse));
     }
     
     
---
+++ src/main/java/com/eeerrorcode/lottomate/domain/dto/lotto/LottoResultResponse.java
@@ -0,0 +1,22 @@
+package com.eeerrorcode.lottomate.domain.dto.lotto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class LottoResultResponse {
+    private Long drawRound; // 마지막 회차 정보입니다. 
+    private LocalDateTime drawDate; // 마지막 회차의 날짜 정보입니다. 
+    private Long totalCount; // 마지막 회차까지의 Row 개수 정보입니다. 
+}
