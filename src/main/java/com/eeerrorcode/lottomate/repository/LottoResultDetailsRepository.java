695bc039b9e358a6d57608a68d3ec3ef530d48c1
DaHnDell <khanman1208@gmail.com>
2025-04-18 16:54:28 +0900

    패키지 구조 변경

--- src/main/java/com/eeerrorcode/lottomate/repository/LottoResultDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eeerrorcode.lottomate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eeerrorcode.lottomate.domain.entity.lotto.LottoResultDetails;
-
-public interface LottoResultDetailsRepository extends JpaRepository<LottoResultDetails, Long>{
-    
-}
