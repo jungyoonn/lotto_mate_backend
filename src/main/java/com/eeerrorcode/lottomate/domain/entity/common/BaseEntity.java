a8e9e6598c94435c8eba909a28b3b24077bface2
jungyoonn <sophia7625@naver.com>
2025-04-18 15:21:09 +0900

    생성 일자, 수정 일자 베이스 엔티티

---
+++ src/main/java/com/eeerrorcode/lottomate/domain/entity/common/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.eeerrorcode.lottomate.domain.entity.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+@Getter
+@ToString
+public class BaseEntity {
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;  
+}
