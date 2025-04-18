bd81a229e6d9c5aa7486d5d69183c32751305bda
jungyoonn <sophia7625@naver.com>
2025-04-18 15:58:55 +0900

    구독, 결제 관련 리포지토리 테스트 코드

---
+++ src/test/java/com/eeerrorcode/lottomate/repository/payment/SubscriptionPlanRepositoryTests.java
@@ -0,0 +1,21 @@
+package com.eeerrorcode.lottomate.repository.payment;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class SubscriptionPlanRepositoryTests {
+  @Autowired
+  private SubscriptionPlanRepository repository;
+
+  @Test
+  public void testRepositoryExists() {
+    assertNotNull(repository);
+  }
+}
