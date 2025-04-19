f4f54a272559ad7a8f81b367c74b3037d99b203e
jungyoonn <sophia7625@naver.com>
2025-04-19 22:44:36 +0900

    결제 관련 서비스(파일만 생성)

---
+++ src/main/java/com/eeerrorcode/lottomate/service/payment/PaymentService.java
@@ -0,0 +1,5 @@
+package com.eeerrorcode.lottomate.service.payment;
+
+public interface PaymentService {
+  
+}
---
+++ src/main/java/com/eeerrorcode/lottomate/service/payment/PaymentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.eeerrorcode.lottomate.service.payment;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Transactional
+@Log4j2
+public class PaymentServiceImpl {
+  
+}
